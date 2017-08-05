9f9287795cd2fad9181f23d785badf0670fb28f5
Rajender <rthakur.dev@gmail.com>
2017-08-05 18:02:25 +0530

    Laravel basic auth setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+                <h1 class="text-center">Welcome to test project</h1>
+                </div>
+        </div>
+    </div>
+</div>
+@endsection
